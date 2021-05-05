e98967fedeff542cb967dbf300b1c6bd9b6ce598
Ni Yunchuan <Niyunchuan7@qq.com>
2021-05-05 13:37:31 +0800

    UART_Receive

---
+++ USIC_UART_Receive/Peripherals_Drive/USIC.h
@@ -0,0 +1,7 @@
+#ifndef _USIC_h
+#define _USIC_h
+
+#include <XMC4400.h>
+
+extern void UART_Init(void);
+#endif
---
+++ USIC_UART_Receive/SYSTEM/Var_Init.h
@@ -0,0 +1,10 @@
+#ifndef _Var_Init_h
+#define _Var_Init_h
+
+#include <XMC4400.h>
+
+#define REC_LENGTH  			200 
+
+extern uint16_t Receive_Data[REC_LENGTH];
+extern uint8_t Receive_Count;
+#endif
