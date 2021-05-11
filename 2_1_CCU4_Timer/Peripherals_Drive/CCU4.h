8b951f412e408fb53e48ada8bd1af99a79953236
Ni Yunchuan <Niyunchuan7@qq.com>
2021-05-11 09:49:42 +0800

    sort

---
+++ 2_1_CCU4_Timer/Peripherals_Drive/CCU4.h
@@ -0,0 +1,8 @@
+#ifndef _CCU4_h
+#define _CCU4_h
+
+#include <XMC4400.h>
+
+extern void CCU4_Init(void);
+extern void CCU40_CC40_Start(void);
+#endif
--- CCU4_Timer/Peripherals_Drive/CCU4.h
+++
@@ -1,8 +0,0 @@
-#ifndef _CCU4_h
-#define _CCU4_h
-
-#include <XMC4400.h>
-
-extern void CCU4_Init(void);
-extern void CCU40_CC40_Start(void);
-#endif
